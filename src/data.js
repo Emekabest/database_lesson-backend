9c6d38eefcae0c44db360b514be891621cf8150b
Dimma <josephemeka2611@gmail.com>
2021-05-26 15:13:51 +0100

    initial commit

---
+++ src/data.js
@@ -0,0 +1,51 @@
+module.exports = {
+  Workers_Data: [
+    {
+      name: "Sami",
+      post: "Web designer",
+      age: "18",
+    },
+
+    {
+      name: "Stanley",
+      post: "Web dev",
+      age: "21",
+    },
+
+    {
+      name: "Joseph",
+      post: "Programmer",
+      age: "18",
+    },
+
+    {
+      name: "Mr sleek",
+      post: "Programmer",
+      age: "30",
+    },
+
+    {
+      name: "Ebuka",
+      post: "Web designer",
+      age: "17",
+    },
+
+    {
+      name: "John",
+      post: "Doctor",
+      age: "21",
+    },
+
+    {
+      name: "Rebecca",
+      post: "Destop Publisher",
+      age: "19",
+    },
+
+    {
+      name: "Rebecca",
+      post: "Destop Publisher",
+      age: "19",
+    },
+  ],
+};
