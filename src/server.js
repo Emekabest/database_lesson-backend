892b117f9ec2aab7409476b8bf11db45cd42fe18
Dimma <josephemeka2611@gmail.com>
2021-05-26 15:51:45 +0100

    add dynamic port

--- src/server.js
+++ src/server.js
@@ -9,6 +9,8 @@ app.get("/api/products", (req, res) => {
   res.send(data.Workers_Data);
 });
 
-app.listen(5000, () => {
-  console.log("serve at localhost:5000");
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`serve at localhost:${port}`);
 });
